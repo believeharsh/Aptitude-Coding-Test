4c077c5c13d9dc7ed38c32ab0f7338d568387633
Believeharsh <bontinue@gmail.com>
2023-11-21 19:27:09 +0530

    added second question into this file

--- Aptitue7.js
+++ Aptitue7.js
@@ -5,3 +5,10 @@
 // 2.Using the Array() constructor: let arr = new Array();
 
 
+// Question 2: How do you access the first and last elements of an array?
+// Answer: You can access the first element of an array using index 0 and the last element using the index array.length - 1. For example:
+
+
+// let arr = [1, 2, 3, 4];
+// let firstElement = arr[0]; // 1
+// let lastElement = arr[arr.length - 1]; // 4
