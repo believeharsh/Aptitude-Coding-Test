78128f9d5fdb8ef361d632728c8dac2584d3ec45
Believeharsh <bontinue@gmail.com>
2023-11-21 19:25:04 +0530

    added Aptitude7.js

---
+++ Aptitue7.js
@@ -0,0 +1,7 @@
+// Question 1: How do you create an empty array in JavaScript?
+// Answer: You can create an empty array in JavaScript using either of the following methods:
+
+// 1.Using array literal syntax: let arr = [];
+// 2.Using the Array() constructor: let arr = new Array();
+
+
