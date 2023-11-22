019d906cae3104233af3ad06f99a88475a201bd8
Believeharsh <bontinue@gmail.com>
2023-11-22 15:12:19 +0530

    added new file aptitude8.js

---
+++ Aptitude8.js
@@ -0,0 +1,12 @@
+// Question 8: How do you concatenate two arrays in JavaScript?
+// Answer: You can concatenate two arrays using the concat() method. For example:
+
+let arr1 = [1, 2];
+
+let arr2 = [3, 4];
+
+let newArr = arr1.concat(arr2);
+
+console.log(newArr); // [1, 2, 3, 4]
+
+// After these basic JavaScript array interview questions, below we have provided some array interview questions in JavaScript for experienced candidates.
