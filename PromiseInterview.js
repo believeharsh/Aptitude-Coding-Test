f5b320f75fb921c9ba916ea799da9610e6ec3f9c
Believeharsh <bontinue@gmail.com>
2023-12-20 16:05:32 +0530

    formated

--- PromiseInterview.js
+++ PromiseInterview.js
@@ -1,2 +1,5 @@
 // What is the Promise in the JavaScrpit?
 // Promise is an objects which represent the eventual value of completion or failure of an particlar asynchronous operation.
+
+// Why we use Promise in JavaScript?
+// in the JavaScript promise are used to handle the asynchronous  operation . Asynchronous operations are the task that may take some time to complete. Rather blocking the execution the of the code white wating for these opeartion to complete, javaScript uses the asynchronous programming to continue executing other tasks white waiting for the asynchronous opeartions to finish. 
